352b8a1ff39a20a352ccadf718b7f9c6b2c86494
= <=>
2021-09-01 12:25:07 -0300

    on development

--- FPS/Common/GameManager.cs
+++ FPS/Common/GameManager.cs
@@ -7,9 +7,15 @@ using UnityEngine.UI;
 namespace FPS {
     public class GameManager : MonoBehaviour {
         [SerializeField] private float _skyboxRotationSpeed = 1f;
-        [SerializeField] private Canvas _playerCanvas;
-        [SerializeField] private TextMesh _playerHealthText;
-        [SerializeField] private TextMesh _playerAmmoText;
+        [SerializeField] private Canvas _playerHUD;
+        [SerializeField] private Canvas _menuHUD;
+        private Dictionary<string, Canvas> _gameCanvas = new Dictionary<string, Canvas>();
+
+        // For each new canvas added to the game, add it to the list in order to be able to hide/show them
+        private void SetGameCanvas() {
+            this._gameCanvas.Add("PlayerHUD", this._playerHUD);
+            this._gameCanvas.Add("MenuHUD", this._menuHUD);
+        }
 
         public void ChangeScene(string sceneName, bool loadAsync = false) {
             if (SceneManager.GetActiveScene().name == sceneName) return;
@@ -27,6 +33,15 @@ namespace FPS {
 
         void Update() {
             this.RenderSkyboxRotation();
+        }
+
+        public void ShowHUD(string hudName) {
+            if (this._gameCanvas.ContainsKey(hudName)) {
+                foreach (KeyValuePair<string, Canvas> dictionaryEntry in this._gameCanvas) {
+                    dictionaryEntry.Value.enabled = false;
+                }
+                this._gameCanvas[hudName].enabled = true;
+            }
         }
     }
 }
